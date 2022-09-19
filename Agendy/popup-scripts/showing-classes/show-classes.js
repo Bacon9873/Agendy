3af76a90c89bd25d3b6a905bae7539d2b7ac6c1e
Daniel <46487826+Dmarky9873@users.noreply.github.com>
2022-09-18 22:01:58 -0400

    Made things work

--- Agendy/popup-scripts/showing-classes/show-classes.js
+++ Agendy/popup-scripts/showing-classes/show-classes.js
@@ -1,11 +1,5 @@
 var day = localStorage.getItem('day')
 
-var holidays = ['2022-10-07', '2022-10-10'];
-var isShownDateWeekend = false
-var isShownDateMonday = false
-var isShownDateSunday = false
-var isShownDateHoliday = false
-
 var classA = localStorage.getItem('class-a')
 var classB = localStorage.getItem('class-b')
 var classC = localStorage.getItem('class-c')
@@ -77,6 +71,29 @@ function dayFourNormal() {
     localStorage.setItem('p6', "F")
 }
 
+function updateDate(daysToAdd) {
+    var today = new Date()
+    newDate = today.setDate(today.getDate() + daysToAdd)
+
+    shownDate = new Date(newDate)
+    if (shownDate.getDay() == 0 || shownDate.getDay() == 6) {
+        isShownDateWeekend = true
+    } else {
+        isShownDateWeekend = false
+    }
+    if (shownDate.getDay() == 1) {
+        isShownDateMonday = true
+    } else {
+        isShownDateMonday = false
+    }
+    if (shownDate.getDay() == 0) {
+        isShownDateSunday = true
+    } else {
+        isShownDateSunday = false
+    }
+    document.getElementById('date').innerHTML = shownDate.toDateString()
+}
+
 
 
 function updateShownClasses() {
@@ -128,115 +145,46 @@ document.addEventListener('DOMContentLoaded', function() {
     }, false)
 }, false)
 
-function updateDate(daysToAdd) {
-    var today = new Date()
-    newDate = today.setDate(today.getDate() + daysToAdd)
-
-    shownDate = new Date(newDate)
-    if (shownDate.getDay() == 0 || shownDate.getDay() == 6) {
-        isShownDateWeekend = true
-    } else {
-        isShownDateWeekend = false
-    }
-    if (shownDate.getDay() == 1) {
-        isShownDateMonday = true
-    } else {
-        isShownDateMonday = false
-    }
-    if (shownDate.getDay() == 0) {
-        isShownDateSunday = true
-    } else {
-        isShownDateSunday = false
-    }
-    document.getElementById('date').innerHTML = shownDate.toDateString()
-
-    // for (let i = 0; i < holidays.length; i++) {
-    //     if (`${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() + 1}` == holidays[i]) {
-    //         isShownDateHoliday = true
-    //     } else {
-    //         isShownDateHoliday = false
-
-    //     }
-    // }
-    // print(isShownDateHoliday)
-
-
-}
-
-// Got this all from stackoverflow
-function workingDaysBetweenDates(d0, d1) {
-    var startDate = parseDate(d0);
-    var endDate = parseDate(d1);
-    // populate the holidays array with all required dates without first taking care of what day of the week they happen
-    // Validate input
-    if (endDate < startDate)
-        return 0;
-
-    var z = 0; // number of days to substract at the very end
-    for (i = 0; i < holidays.length; i++) {
-        var cand = parseDate(holidays[i]);
-        var candDay = cand.getDay();
-
-        if (cand >= startDate && cand <= endDate && candDay != 0 && candDay != 6) {
-            // we'll only substract the date if it is between the start or end dates AND it isn't already a saturday or sunday
-            z++;
-        }
-
-    }
-    // Calculate days between dates
-    var millisecondsPerDay = 86400 * 1000; // Day in milliseconds
-    startDate.setHours(0, 0, 0, 1); // Start just after midnight
-    endDate.setHours(23, 59, 59, 999); // End just before midnight
-    var diff = endDate - startDate; // Milliseconds between datetime objects    
-    var days = Math.ceil(diff / millisecondsPerDay);
-
-    // Subtract two weekend days for every week in between
-    var weeks = Math.floor(days / 7);
-    days = days - (weeks * 3);
-
-    // Handle special cases
-    var startDay = startDate.getDay();
-    var endDay = endDate.getDay();
-
-    // Remove weekend not previously removed.   
-    if (startDay - endDay > 1)
-        days = days - 2;
-
-    // Remove start day if span starts on Sunday but ends before Saturday
-    if (startDay == 0 && endDay != 6)
-        days = days - 1
-
-    // Remove end day if span ends on Saturday but starts after Sunday
-    if (endDay == 6 && startDay != 0)
-        days = days - 1
-
-    // substract the holiday dates from the original calculation and return to the DOM
-    return days - z;
-}
-
-function parseDate(input) {
-    // Transform date from text to date
-    var parts = input.match(/(\d+)/g);
-    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-    return new Date(parts[0], parts[1] - 1, parts[2]); // months are 0-based
-}
-
 const date = new Date()
 
-const daysSinceStart = workingDaysBetweenDates('2022-09-07', '2022-09-18' /*`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() + 1}`*/ );
+var month = date.getMonth()
+if (`${date.getMonth() + 1}`.length == 1) {
+    month = '0' + `${date.getMonth() + 1}`
+}
 
-newDay = daysSinceStart % 4
-if (newDay == 0) {
-    newDay = 4
+var dayOfMonth = date.getDate()
+if (`${date.getDate() + 1}`.length == 1) {
+    dayOfMonth = '0' + `${date.getDate() + 1}`
+}
+
+todaysDate = `${date.getFullYear()}${month}${dayOfMonth}`
+
+console.log(todaysDate);
+
+async function getJSON() {
+    return fetch('/popup-scripts/showing-classes/dates.json')
+        .then((response) => response.json())
+        .then((responseJson) => { return responseJson });
 }
 
 
-localStorage.setItem('day', newDay)
+async function updateThings() {
+    const dayNum = await this.getJSON()
+    var foundDay = false
 
+    for (let i = 0; i < dayNum.dates.length; i++) {
+        if (dayNum.dates[i] == todaysDate) {
+            day = dayNum.dates[c + 1]
+            foundDay = true
+        }
+    }
+    localStorage.setItem('day', day)
 
+    day = localStorage.getItem('day')
+    updateShownClasses()
+    updateDate(0)
+}
 
-day = localStorage.getItem('day')
-updateShownClasses()
-updateDate(0)
+updateThings()
 
 // TODO: Make system to load arbitrary date
